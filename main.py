c316408983f6fe354ab77094510599d858503fba
Alonso.C.Pizarro <alonsocuevaspizarro@gmail.com>
2024-08-24 21:05:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Descrption: ste es un archivo de prueba para aprender Python
+
+from time import sleep, time
+CONSTANTE = 200
+
+print("/////////////////////////////////////")
+print()
+
+edad = input("Ingrese su edad: ")
+print("Su edad es: ", edad)
+print("es del tipo", type(edad))
+
+edad = int(float(edad)) #"1,5" -> 1.5 -> 1
+print("Su edad es: ", edad)
+print("es del tipo", type(edad))
+
