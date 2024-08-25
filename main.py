d36ec49d8a77e3e62d5713e64f64709603e17d21
Alonso.C.Pizarro <alonsocuevaspizarro@gmail.com>
2024-08-25 00:43:11 -0400

    comenta parte del codigo en main

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ CONSTANTE = 200
 print("/////////////////////////////////////")
 print()
 
-edad = input("Ingrese su edad: ")
-print("Su edad es: ", edad)
-print("es del tipo", type(edad))
+# edad = input("Ingrese su edad: ")
+# print("Su edad es: ", edad)
+# print("es del tipo", type(edad))
 
-edad = int(float(edad))  # "1,5" -> 1.5 -> 1
-print("Su edad es: ", edad)
-print("es del tipo", type(edad))
+# edad = int(float(edad))  # "1,5" -> 1.5 -> 1
+# print("Su edad es: ", edad)
+# print("es del tipo", type(edad))
 
 # esto es un comentario de prueba!
+
+# este codigo no staba funcionando, esto es un hotfix
